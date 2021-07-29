77907d6ffc8d984e03e02b52a444169d5a10bf10
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2021-07-29 07:48:00 +0000

    Fix bug where all colors go off at once. Solution found on stackowerflow, by Cooper Buckingham. Needed to ad setTimeout after the begining of for loop, before call for colorBlink function

--- assets/js/color-game.js
+++ assets/js/color-game.js
@@ -13,47 +13,45 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Atsitiktinis skaicius - ' + num);
         console.log('Atsitiktiniu skaicius seka - ' + cicleMemmory);
 
-        startGame(cicleMemmory, cicle);
+        startGame(cicleMemmory);
     });
 });
 
-function startGame(cicleMemmory, cicle) {
-    let i = 0;
-    while (cicle > 0) {
+function startGame(cicleMemmory) {
+    for (var i = 0; i < cicleMemmory.length; i++) {
         let num = cicleMemmory[i];
-        i++;
-
-        if (num == 1) {
-            document.getElementById('red-button').style.backgroundColor = 'red';
-            setTimeout(function () {
-                document.getElementById('red-button').style.backgroundColor = ''
-            }, 500);
-        } else {
-            if (num == 2) {
-                document.getElementById('blue-button').style.backgroundColor = 'blue';
-                setTimeout(function () {
-                    document.getElementById('blue-button').style.backgroundColor = ''
-                }, 500);
-
-            } else {
-                if (num == 3) {
-                    document.getElementById('green-button').style.backgroundColor = 'green';
-                    setTimeout(function () {
-                        document.getElementById('green-button').style.backgroundColor = ''
-                    }, 500);
-
-                } else {
-                    if (num == 4) {
-                        document.getElementById('yellow-button').style.backgroundColor = 'yellow';
-                        setTimeout(function () {
-                            document.getElementById('yellow-button').style.backgroundColor = ''
-                        }, 500);
-
-                    }
-                }
-            }
-        }
-        console.log(num);
-        cicle--;
+        setTimeout(function () {
+            colorBlink(num);
+            //console.log(cicleMemmory[i]);
+        }, 500 * i);
     }
+
+}
+
+function colorBlink(num) {
+    if (num == 1) {
+        document.getElementById('red-button').style.backgroundColor = 'red';
+        setTimeout(function () {
+            document.getElementById('red-button').style.backgroundColor = '';
+        }, 500);
+    }
+    if (num == 2) {
+        document.getElementById('blue-button').style.backgroundColor = 'blue';
+        setTimeout(function () {
+            document.getElementById('blue-button').style.backgroundColor = '';
+        }, 500);
+    }
+    if (num == 3) {
+        document.getElementById('green-button').style.backgroundColor = 'green';
+        setTimeout(function () {
+            document.getElementById('green-button').style.backgroundColor = '';
+        }, 500);
+    }
+    if (num == 4) {
+        document.getElementById('yellow-button').style.backgroundColor = 'yellow';
+        setTimeout(function () {
+            document.getElementById('yellow-button').style.backgroundColor = '';
+        }, 500);
+    }
+    console.log(num);
 }
